0bd2622fb839a073810acc43816b3767915bff09
jarubank <67811693+jarubank@users.noreply.github.com>
2020-07-13 23:20:49 +0700

    Add files via upload

---
+++ src/interfacesystech_log_node.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import json
+import re
+import time
+
+import requests
+import rospy
+from pymongo import MongoClient
+from interfacesystech_log.msg import LogRecord
+
+c = MongoClient()
+database_name = rospy.get_param("~database_name", "interfacesystech_log")
+db = c[database_name]
+CODE_NAME = rospy.get_param('/galileo/code_name', "")
+VERSION = rospy.get_param('/galileo/version', "0.0.0")
+
+def get_id(sharplink_log):
+    log_file = open(sharplink_log)
+    contents = log_file.read()
+    log_file.close()
+    mid_search = re.search(r"[0-9]+,\sID:\s(?P<id>[0-9A-F]{76})", contents)
+    mid = mid_search.group("id")
+    return mid
+
+
+def insert_log_record(record):
+    global CODE_NAME
+    db[record.collection_name].insert({
+        "timestamp": record.stamp.to_nsec() / 1000 / 1000,
+        "record": json.loads(record.record)
+    })
+    try:
+        server_url = rospy.get_param("~server_url", "http://127.0.0.1:9999")
+        sharplink_log = rospy.get_param("~sharplink_log", "")
+        if sharplink_log == "":
+            return
+        res = requests.get(server_url, params={
+            "id": get_id(sharplink_log),
+            "collection": record.collection_name,
+        }, timeout=5)
+        res = json.loads(res.content.decode("utf-8"))
+        timestamp = 0
+        if res is not None:
+            timestamp = res["timestamp"]
+        # query data need to send
+        data_to_send = list(db[record.collection_name].find(
+            {"timestamp": {"$gt": timestamp}}, {"_id": 0}
+        ))
+        if len(data_to_send) == 0:
+            return
+        for data in data_to_send:
+            data["collection"] = record.collection_name
+            data["record"]["id"] = get_id(sharplink_log)
+            data["record"]["version"] = VERSION
+            data["record"]["codename"] = CODE_NAME
+        # send data to server
+        res = requests.post(server_url, json=data_to_send)
+        res = json.loads(res.content.decode("utf-8"))
+        if len(res) < len(data_to_send):
+            rospy.logwarn("some records failed to insert")
+
+    except Exception as e:
+        rospy.loginfo("Connect to remote server failed")
+        rospy.loginfo(e)
+
+
+if __name__ == "__main__":
+    rospy.init_node("interfacesystech_logger_node")
+    sub = rospy.Subscriber("/interfacesystech_log", LogRecord,
+                           insert_log_record, queue_size=10)
+    while not rospy.is_shutdown():
+        CODE_NAME = rospy.get_param('/galileo/code_name', "")
+        VERSION = rospy.get_param('/galileo/version', "0.0.0")
+        time.sleep(5)
---
+++ src/interfacesystech_log_server.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import json
+import time
+
+import pymongo
+import web
+from bson import json_util
+from pymongo import MongoClient
+import threading
+import requests
+from functools import lru_cache
+
+c = MongoClient()
+db = c["interfacesystech_log_server"]
+
+urls = (
+    '/', 'index'
+)
+
+
+class index:
+
+    def GET(self):
+        user_data = web.input()
+        if "id" not in user_data or "collection" not in user_data:
+            return json.dumps({
+                "status": "error",
+                "description": "id and collection is required"
+            }, indent=4)
+
+        # 查找到最新的时间戳
+        record = db[user_data.collection].find_one({"id": user_data.id}, sort=[
+            ("timestamp", pymongo.DESCENDING)])
+        return json.dumps(record, default=json_util.default)
+
+    def POST(self):
+        data = web.data().decode("utf-8")
+        try:
+            data = json.loads(data)
+        except Exception as e:
+            print(e)
+            data = None
+        if data is None:
+            return json.dumps({
+                "status": "error",
+                "description": "not a valid json format"
+            })
+
+        inserted_records = []
+        timestamps = {}
+        for received_record in data:
+            # 检查数据格式
+            if "timestamp" not in received_record or "collection" not in received_record or "record" not in received_record:
+                continue
+            if "id" not in received_record["record"]:
+                continue
+            # 查找到最新的时间戳
+            if received_record["collection"] not in timestamps:
+                db_record = db[received_record["collection"]].find_one({"id": received_record["record"]["id"]}, sort=[
+                    ("timestamp", pymongo.DESCENDING)])
+                if db_record is not None:
+                    timestamps[received_record["collection"]] = db_record["timestamp"]
+            # 收到数据并不比本地数据新
+            if received_record["collection"] in timestamps and timestamps[received_record["collection"]] > received_record["timestamp"]:
+                continue
+            # 保存数据
+            received_record["record"]["timestamp"] = received_record["timestamp"]
+            received_record["record"]["ip"] = web.ctx.env.get('HTTP_X_FORWARDED_FOR')
+            received_record["record"]["location"] = self.get_phy_addr(received_record["record"]["ip"])
+            db[received_record["collection"]].insert(received_record["record"])
+            inserted_records.append(received_record["record"])
+        return json.dumps(inserted_records, indent=4, default=json_util.default)
+
+    @lru_cache(maxsize=1024*1024)
+    def get_phy_addr(self, ip):
+        try:
+            req = requests.post("http://interfacesystech.bwbot.org/ips", data={"ip": ip})
+        except Exception:
+            return ""
+        if req.json()[0] is None:
+            return ""
+        return req.json()[0]
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    threading._start_new_thread(app.run, ())
+    while True:
+        time.sleep(1)
